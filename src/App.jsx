65279a746e4d4a38157a21076ca9072d92c5930c
Vova Polishchuk <volodimirpolishchuck@gmail.com>
2025-10-29 21:32:16 +0200

    update 6.20: fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './request.js'
 
 const App = () => {
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  const { isPending, isError, data } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false
+  })
+
+  if (isPending)
+    return <span>Loading...</span>
+
+  if (isError)
+    return <span>anecdote service not available due to problems in server</span>
+
 
   return (
     <div>
@@ -21,7 +28,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
 
-      {anecdotes.map((anecdote) => (
+      {data.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App'
 
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>
 )
